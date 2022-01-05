4ff58a327c450e15a8097188c0e20de302a27133
Vadim Kalinsky <vkalinsky@epiphan.com>
2022-01-05 17:41:47 -0500

    Reordered demo items.

--- examples/full_lifecycle.py
+++ examples/full_lifecycle.py
@@ -1,11 +1,16 @@
+"""
+This is an example of Epiphan API usage.
+
+Set the environment variables EPIPHAN_API_TOKEN and PAIRING_CODE to run the example.
+
+If PAIRING_CODE is not set, the example will list all devices and exit.
+"""
+
 import os
 import time
 import logging
 
-from epiphancloud.api import API
-from epiphancloud.exceptions import EpiphanCloudException
-
-# Setting up the logger
+from epiphancloud import API, exceptions
 
 logging.basicConfig(
     level=logging.INFO,
@@ -14,7 +19,7 @@ logging.basicConfig(
 def use_api():
     API_TOKEN = os.environ["EPIPHAN_API_TOKEN"]
     API_HOST = os.environ["EPIPHAN_HOST"] or "go.epiphan.cloud"
-    PAIRING_CODE = os.environ["PAIRING_CODE"] or "go.epiphan.cloud"
+    PAIRING_CODE = os.environ.get("PAIRING_CODE")
 
     if API_TOKEN is None:
         raise Exception("EPIPHAN_API_TOKEN environment variable is not set")
@@ -27,10 +32,16 @@ def use_api():
     # Pairing the device (if needed)
     if PAIRING_CODE is None:
         logging.info("PAIRING_CODE environment variable is not set, pairing skipped")
+
+        # Getting all devices
+        devices = api.Devices.get_all()
+        for device in devices:
+            logging.info(device)
+
         exit(0)
 
-    device_name = f"Test device {PAIRING_CODE}"
-    api.Devices.pair(PAIRING_CODE, f"API example {PAIRING_CODE}")
+    device_name = f"API example {PAIRING_CODE}"
+    api.Devices.pair(PAIRING_CODE, device_name)
     logging.info("Pairing completed")
 
     # Getting the device
@@ -49,15 +60,10 @@ def use_api():
     # Deleting the device
     device.delete()
 
-    # Getting all devices
-    devices = api.Devices.get_all()
-    for device in devices:
-        logging.info("Device: %s", device.id)
-
 if __name__ == "__main__":
     try:
         use_api()
-    except EpiphanCloudException as e:
+    except exceptions.EpiphanCloudException as e:
         logging.error("Cloud error: %s", e)
     except Exception as e:
         logging.error(e)
